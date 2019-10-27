00182e1cf7c701eab13c1c35d5068dcea2d073ad
Frances Cohen <fcohen26@gmail.com>
2019-10-27 19:54:54 -0400

    username not unique

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
 // create a UserSchema with a title field
 const UserSchema = new Schema({
-  username: { type: String, unique: true },
+  username: { type: String, unique: false },
 });
 // create UserModel class from schema
 const UserModel = mongoose.model('Users', UserSchema);
