0ff526ebbc8654f1cebde39e0bb35475f306ea87
Frances Cohen <fcohen26@gmail.com>
2019-11-12 17:15:49 -0500

    quick fix

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -3,12 +3,6 @@
 import jwt from 'jwt-simple';
 import dotenv from 'dotenv';
 import User from '../models/user_model';
-import Survey from '../models/survey_model';
-import Basic from '../models/surveymodels/basic_model';
-import CS from '../models/surveymodels/cs_model';
-import Demographic from '../models/surveymodels/demographic_model';
-import Education from '../models/surveymodels/education_model';
-import Personality from '../models/surveymodels/personality_model';
 
 dotenv.config({ silent: true });
 
