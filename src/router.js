68ad31d2f924903a2b5bd950b171fbb5b8fb085a
Annika Kouhia <annika.j.kouhia.20@dartmouth.edu>
2020-01-11 14:10:14 -0500

    can get from/to and also just all chats between

--- src/router.js
+++ src/router.js
@@ -25,6 +25,7 @@ router.post('/events/rsvp/:id', EventController.rsvpEvent);
 router.post('/events/unrsvp/:id', EventController.unrsvpEvent);
 router.post('/chats/add', ChatController.addChat);
 router.get('/chats/getBetween', ChatController.getBetween);
+router.get('/chats/getToFrom', ChatController.getToFrom);
 router.put('/users/survey/:id', UserController.addToSurvey);
 
 export default router;
