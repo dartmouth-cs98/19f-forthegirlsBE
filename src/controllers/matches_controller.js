b76572e17357c1e17308290be6726638c967e37f
Frances Cohen <fcohen26@gmail.com>
2020-01-26 11:52:14 -0500

    introduce concept of score

--- src/controllers/matches_controller.js
+++ src/controllers/matches_controller.js
@@ -8,45 +8,21 @@ import User from '../models/user_model';
 dotenv.config({ silent: true });
 
 export const addMatch = (req, res) => {
-  // let exists = false;
   const { user1 } = req.body;
   const { user2 } = req.body;
-  // const { matched } = req.body;
-  // const match = new Match();
   User.findOne({ username: user1 }).then((resp) => {
     User.findOne({ username: user2 }).then((resp2) => {
       Match.find({ user1: resp.id, user2: resp2.id }).then((response) => {
         if (response.length !== 0) {
-          // console.log('RESPONSE');
-          // console.log(response);
           response[0].matched = true;
           response[0].save();
           res.json('match added');
-          // exists = true;
         }
         Match.find({ user1: resp2.id, user2: resp.id }).then((response2) => {
           if (response2.length !== 0) {
-            // exists = true;
-            // console.log('RESPONSE 2');
-            // console.log(response2);
             response2[0].matched = true;
             res.json('match added');
           }
-          // if (exists === false) {
-          //   // match.user1 = resp.id;
-          //   // match.user2 = resp2.id;
-          //   // match.matched = matched;
-          //   // match.save()
-          //   //   .then((saveResp) => {
-          //   //     res.json(saveResp);
-          //   //   })
-          //   //   .catch((error) => {
-          //   //     console.log(error);
-          //   //   });
-          //   res.json('error');
-          // } else {
-          //   res.json('Match exists');
-          // }
         });
       });
     });
@@ -54,9 +30,6 @@ export const addMatch = (req, res) => {
 };
 
 export const removeMatch = (req, res) => {
-  // const { user1 } = req.body;
-  // const { user2 } = req.body;
-  // const match = new Match();
   Match.deleteOne({ _id: req.params.id }).then((result) => {
     res.json({ result });
   });
@@ -143,26 +116,15 @@ export const getMatches = (req, res) => {
 };
 
 export const getMatchId = (req, res) => {
-  // let exists = false;
-  // const { user1 } = req.params.id1;
-  // const { user2 } = req.params.id2;
-  // const match = new Match();
-  // User.findById({ _id: req.params.id1 }).then((resp) => {
-  // User.findById({ _id: req.params.id2 }).then((resp2) => {
   Match.find({ user1: req.params.id1, user2: req.params.id2 }).then((response) => {
     if (response.length !== 0) {
-      // exists = true;
       res.json(response[0].id);
     } else {
       Match.find({ user1: req.params.id2, user2: req.params.id1 }).then((response2) => {
         if (response2.length !== 0) {
-          // exists = true;
           res.json(response2[0].id);
         }
-        // res.json('Match does not exist');
       });
     }
   });
-  // });
-  // });
 };
